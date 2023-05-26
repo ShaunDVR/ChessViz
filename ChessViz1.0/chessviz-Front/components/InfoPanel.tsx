77214a067f71e787dc8e2df8a8eba2a86c1364ef
Shaun Reilly <104159375+IronhackShaun@users.noreply.github.com>
2023-05-26 23:42:03 +0100

    path issues with capitalization, again, again

--- ChessViz1.0/chessviz-Front/components/InfoPanel.tsx
+++ ChessViz1.0/chessviz-Front/components/InfoPanel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/InfoPanel.module.css";
+import styles from "../styles/InfoPane.module.css";
 
 type InfoPanelProps = {
   gameSessionRoom: string;
